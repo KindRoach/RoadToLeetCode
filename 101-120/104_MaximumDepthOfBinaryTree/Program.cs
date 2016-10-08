2741bc2358c7ccc53959a7a4081de11c1ed7d03a
Kind Roach <kindroach@hotmail.com>
2016-10-08 23:25:55 +0800

    刷刷刷

---
+++ 101-120/104_MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104_MaximumDepthOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ 381-400/389_FindTheDifference/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _389_FindTheDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public char FindTheDifference(string s, string t)
+        {
+            var appearS = new int[26];
+            var appearT = new int[26];
+            char ans = ' ';
+            for (int i = 0; i < s.Length; i++)
+            {
+                appearS[s[i] - 'a']++;
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                appearT[t[i] - 'a']++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (appearS[i] < appearT[i])
+                {
+                    ans = (char)('a' + i);
+                }
+            }
+            return ans;
+        }
+
+        public char FindTheDifference2(string s, string t)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                ans = ans ^ s[i];
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                ans = ans ^ t[i];
+            }
+            return (char)ans;
+        }
+    }
+}
