c7ebf400072c9c12630eeefa3f0a7f78090cb1a5
Kind Roach <kindroach@hotmail.com>
2016-10-11 20:11:49 +0800

    2016/10/11打卡

---
+++ 221_240/226_InvertBinaryTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226_InvertBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root != null)
+            {
+                root.left = InvertTree(root.left);
+                root.right = InvertTree(root.right);
+
+                var temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+            }
+            return root;
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+
+}
---
+++ 241-260/260_SingleNumberIII/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _260_SingleNumberIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] SingleNumber(int[] nums)
+        {
+            int res = 0;
+            foreach (var item in nums)
+            {
+                res = res ^ item;
+            }
+            int flag = 1;
+            while ((res & flag) == 0)
+            {
+                flag = flag << 1;
+            }
+            int res1 = 0, res2 = 0;
+            foreach (var item in nums)
+            {
+                if ((item & flag) == 0)
+                {
+                    res1 = res1 ^ item;
+                }
+                else
+                {
+                    res2 = res2 ^ item;
+                }
+            }
+            return new int[] { res1, res2 };
+        }
+    }
+}
