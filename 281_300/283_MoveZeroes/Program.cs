096725f01d6b7b3aef92b1a91e45a52b2c696d9a
Kind Roach <kindroach@hotmail.com>
2016-10-12 23:49:22 +0800

    今天只做了两道……

---
+++ 281_300/283_MoveZeroes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _283_MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ints = new int[] { 0, 0, 1 };
+            MoveZeroes(ints);
+        }
+
+        static void MoveZeroes(int[] nums)
+        {
+            int last0 = nums.Count();
+            for (int i = 0; i < last0; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    last0--;
+                    i--;
+                    for (int j = i; j < last0; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+                    nums[last0] = 0;
+                }
+            }
+        }
+
+        // O(N)!!!!!!!!
+        static void MoveZeroes2(int[] nums)
+        {
+            int numZeroes = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] == 0)
+                {
+                    numZeroes++;
+                }
+                else
+                {
+                    nums[i - numZeroes] = nums[i];
+                }
+            }
+            for (int i = nums.Count() - numZeroes; i < nums.Count(); i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
---
+++ 401_420/404_SumOfLeftLeaves/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _404_SumOfLeftLeaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int leftSum;
+            if (root.left != null && root.left.left == null && root.left.right == null)
+            {
+                leftSum = root.left.val;
+            }
+            else
+            {
+                leftSum = SumOfLeftLeaves(root.left);
+            }
+
+            return leftSum + SumOfLeftLeaves(root.right);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
