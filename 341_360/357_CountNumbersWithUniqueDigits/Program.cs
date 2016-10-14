81fa020cd1dbfddce3f2422fb5de038a8a371a5d
Kind Roach <kindroach@hotmail.com>
2016-10-14 19:41:54 +0800

    今天的题好水，大都直接在网页上写的

---
+++ 341_360/357_CountNumbersWithUniqueDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _357_CountNumbersWithUniqueDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountNumbersWithUniqueDigits(4);
+        }
+
+        static int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 10;
+            if (n == 2) return 91;
+            int sum = 9;
+            int an = 9;
+            for (int i = 2; i < n; i++)
+            {
+                an = an * 10 + (9 * (int)Math.Pow(10, i - 1) - an) * i;
+                sum = sum + an;
+                Console.WriteLine($"{an}  {sum}");
+            }
+            return (int)Math.Pow(10, n) - sum;
+        }
+    }
+}
---
+++ 381-400/392_IsSubsequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _392_IsSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool IsSubsequence(string s, string t)
+        {
+
+        }
+    }
+}
