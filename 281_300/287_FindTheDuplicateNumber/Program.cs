924054b4e272a8e240866e4cf83fdbacc7277b73
Kind Roach <kindroach@hotmail.com>
2016-10-17 22:56:49 +0800

    苟

---
+++ 281_300/287_FindTheDuplicateNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _287_FindTheDuplicateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int FindDuplicate(int[] nums)
+        {
+            int l = 1, r = nums.Length - 1;
+            int count, mid;
+            while (l < r)
+            {
+                mid = (l + r) / 2;
+                count = 0;
+                foreach (var item in nums)
+                {
+                    if (item <= mid)
+                    {
+                        count++;
+                    }
+                }
+                if (count <= mid)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid;
+                }
+            }
+            return l;
+        }
+    }
+}
---
+++ 301_320/318_MaximumProductOfWordLengths/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _318_MaximumProductOfWordLengths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int MaxProduct(string[] words)
+        {
+            var val = new int[words.Count()];
+            for (int i = 0; i < words.Count(); i++)
+            {
+                foreach (var item in words[i])
+                {
+                    val[i] |= 1 << (item - 'a');
+                }
+            }
+            int maxPro = 0;
+            for (int i = 0; i < words.Count(); i++)
+            {
+                for (int j = 0; j < words.Count(); j++)
+                {
+                    if ((val[i] & val[j]) == 0)
+                    {
+                        maxPro = Math.Max(maxPro, words[i].Length * words[j].Length);
+                    }
+                }
+            }
+            return maxPro;
+        }
+    }
+}
